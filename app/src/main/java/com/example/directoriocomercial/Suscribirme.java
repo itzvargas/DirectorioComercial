e31d752e96bddc2f36700ba9e8dcc055a51f4255
Itzel Vargas <63820823+itzvargas@users.noreply.github.com>
2020-10-09 14:47:55 -0500

    Añadí referencias de los EditText y el Botón en Suscribirme.

--- app/src/main/java/com/example/directoriocomercial/Suscribirme.java
+++ app/src/main/java/com/example/directoriocomercial/Suscribirme.java
@@ -3,9 +3,15 @@ package com.example.directoriocomercial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Suscribirme extends AppCompatActivity {
+public class Suscribirme extends AppCompatActivity implements View.OnClickListener {
 
+    EditText nombre,fecha,email,telefono,face;
+    Button suscr;
     String ruta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,34 @@ public class Suscribirme extends AppCompatActivity {
         setTitle(getString(R.string.menu_suscribirme));
 
         Bundle bolsaR = getIntent().getExtras();
+        ruta = bolsaR.getString("RUTA");
+
+        nombre = (EditText)findViewById(R.id.edt_nombreU);
+        fecha = (EditText)findViewById(R.id.edt_fechaNU);
+        email = (EditText)findViewById(R.id.edt_emailU);
+        telefono = (EditText)findViewById(R.id.edt_telefonoU);
+        face = (EditText)findViewById(R.id.edt_faceU);
+        suscr = (Button)findViewById(R.id.btn_suscribirme);
+
+        suscr.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String nom,fec,em,telef,fac;
+        nom = nombre.getText().toString();
+        fec = fecha.getText().toString();
+        em = email.getText().toString();
+        telef = telefono.getText().toString();
+        fac = face.getText().toString();
+
+        if(v.getId() == R.id.btn_suscribirme){
+            if(!nom.isEmpty() && !fec.isEmpty() && !em.isEmpty() && !telef.isEmpty()){
+
+            }
+            else {
+                Toast.makeText(this, "Faltan campos por llenar",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
