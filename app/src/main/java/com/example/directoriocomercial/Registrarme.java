5b5e70f831730c313a2721dae2a4135f758382d5
Itzel Vargas <63820823+itzvargas@users.noreply.github.com>
2020-10-11 13:42:27 -0500

    Declaración de elementos en Registrarme.

--- app/src/main/java/com/example/directoriocomercial/Registrarme.java
+++ app/src/main/java/com/example/directoriocomercial/Registrarme.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Registrarme extends Fragment {
+public class Registrarme extends Fragment implements View.OnClickListener {
 
+    Spinner eventos;
+    TextView info;
+    Button registro;
 
     public Registrarme() {
         // Required empty public constructor
@@ -27,7 +33,17 @@ public class Registrarme extends Fragment {
         // Inflate the layout for this fragment
         rootview = inflater.inflate(R.layout.fragment_registrarme, container, false);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.menu_registrarme));
+        eventos = (Spinner)rootview.findViewById(R.id.sp_eventos);
+        info = (TextView)rootview.findViewById(R.id.txt_info_evento);
+        registro = (Button)rootview.findViewById(R.id.btn_registrarme);
+        registro.setOnClickListener(this);
         return rootview;
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_registrarme){
+
+        }
+    }
 }
