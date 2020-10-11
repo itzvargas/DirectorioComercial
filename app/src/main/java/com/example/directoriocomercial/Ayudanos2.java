a5a3fdc9976b123024535916aacb86b86453d891
Itzel Vargas <63820823+itzvargas@users.noreply.github.com>
2020-10-11 14:45:29 -0500

    Declaración de elementos en Ayudanos a mejorar.

--- app/src/main/java/com/example/directoriocomercial/Ayudanos2.java
+++ app/src/main/java/com/example/directoriocomercial/Ayudanos2.java
@@ -2,14 +2,45 @@ package com.example.directoriocomercial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Ayudanos2 extends AppCompatActivity {
+public class Ayudanos2 extends AppCompatActivity implements View.OnClickListener {
 
+    EditText nombre,correo,coment;
+    Button enviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayudanos2);
         setTitle(getString(R.string.menu_ayudanos));
+        nombre = (EditText)findViewById(R.id.edt_nom);
+        correo = (EditText)findViewById(R.id.edt_correo);
+        coment = (EditText)findViewById(R.id.edt_comentario);
+        enviar = (Button)findViewById(R.id.btn_enviar_comentario);
+        enviar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String nom, corr, com;
+        Intent intent;
+        if(v.getId() == R.id.btn_enviar_comentario){
+            nom = nombre.getText().toString();
+            corr = correo.getText().toString();
+            com = coment.getText().toString();
+            if(!nom.isEmpty() && !corr.isEmpty() && !com.isEmpty()){
+                intent = new Intent(Ayudanos2.this, Inicio_invitado.class);
+                startActivity(intent);
+                finish();
+            }
+            else {
+                Toast.makeText(this, "Faltan campos por llenar",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/directoriocomercial/Ayudenos.java
+++ app/src/main/java/com/example/directoriocomercial/Ayudenos.java
@@ -1,6 +1,8 @@
 package com.example.directoriocomercial;
 
 
+import android.app.UiAutomation;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,24 +10,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabItem;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Ayudenos extends Fragment {
+public class Ayudenos extends Fragment implements View.OnClickListener {
 
+    EditText comentario;
+    Button enviar;
 
     public Ayudenos() {
         // Required empty public constructor
     }
 
-
+    View rootview;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ayudenos, container, false);
+        rootview = inflater.inflate(R.layout.fragment_ayudenos, container, false);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.menu_ayudanos));
+        comentario = (EditText)rootview.findViewById(R.id.edt_comentarioU);
+        enviar = (Button)rootview.findViewById(R.id.btn_enviarCU);
+        enviar.setOnClickListener(this);
+        return rootview;
     }
 
+    @Override
+    public void onClick(View v) {
+        String com;
+        if(v.getId() == R.id.btn_enviarCU){
+            com = comentario.getText().toString();
+            if(!com.isEmpty()){
+                comentario.setText("");
+                Toast.makeText(getContext(), "Gracias por tu comentario",Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(getContext(), "Escribe tu comentario",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
