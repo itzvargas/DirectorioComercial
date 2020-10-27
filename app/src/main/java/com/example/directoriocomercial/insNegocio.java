7dbc243095233cb5146442811734b29fd322b62a
Itzel Vargas <63820823+itzvargas@users.noreply.github.com>
2020-10-27 13:27:27 -0600

    Correccion de informacion de los Negocios.

--- app/src/main/java/com/example/directoriocomercial/insNegocio.java
+++ app/src/main/java/com/example/directoriocomercial/insNegocio.java
@@ -23,13 +23,13 @@ import android.widget.Toast;
 public class insNegocio extends Fragment implements CheckBox.OnClickListener {
 
     CheckBox sociales;
-    int id_propietario[] = {R.id.edt_nombreUsuario,R.id.edt_telefonoUsuario,R.id.edt_emailUsuario,R.id.edt_fechaNacUsuario,R.id.edt_faceUsuario};
-    int id_negocio[] = {R.id.edt_denominacion,R.id.edt_giro,R.id.edt_productos};
+    //int id_propietario[] = {R.id.edt_nombreUsuario,R.id.edt_telefonoUsuario,R.id.edt_emailUsuario,R.id.edt_fechaNacUsuario,R.id.edt_faceUsuario};
+    int id_negocio[] = {R.id.edt_denominacion,R.id.edt_giro,R.id.edt_descripcion,R.id.edt_productos};
     int id_domicilio[] = {R.id.edt_calle,R.id.edt_noInt,R.id.edt_noExt,R.id.edt_colonia,R.id.edt_codigoPos,R.id.edt_municipio,R.id.edt_estado};
     int id_contacto[] = {R.id.edt_emailNeg,R.id.edt_telefonoNeg,R.id.edt_horaA,R.id.edt_horaC,R.id.edt_dias};
     EditText pagina,faceN,instaN;
-    EditText propietario[] = new EditText[5];
-    EditText negocio[] = new EditText[3];
+    //EditText propietario[] = new EditText[5];
+    EditText negocio[] = new EditText[4];
     EditText domicilio[] = new EditText[7];
     EditText contacto[] = new EditText[5];
     Button inscr;
@@ -47,9 +47,9 @@ public class insNegocio extends Fragment implements CheckBox.OnClickListener {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_ins_negocio, container, false);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.menu_inscribir));
-        for (int i = 0; i<5; i++)
-            propietario[i] = (EditText) rootView.findViewById(id_propietario[i]);
-        for (int i = 0; i<3; i++)
+        //for (int i = 0; i<5; i++)
+            //propietario[i] = (EditText) rootView.findViewById(id_propietario[i]);
+        for (int i = 0; i<4; i++)
             negocio[i] = (EditText) rootView.findViewById(id_negocio[i]);
         for (int i = 0; i<7; i++)
             domicilio[i] = (EditText) rootView.findViewById(id_domicilio[i]);
@@ -68,9 +68,9 @@ public class insNegocio extends Fragment implements CheckBox.OnClickListener {
     @Override
     public void onClick(View v) {
         //Propietario
-        String nombre,telefono,email,fecha,face;
+        //String nombre,telefono,email,fecha,face;
         //Negocio
-        String denom,giro,producto;
+        String denom,giro,descrip,producto;
         //Domicilio
         String calle,noI,noE,colonia,codigo,munic,estado;
         //Contacto
@@ -89,15 +89,16 @@ public class insNegocio extends Fragment implements CheckBox.OnClickListener {
             }
         }
         if(v.getId() == R.id.btn_inscribir){
-            nombre = propietario[0].getText().toString();
+            /*nombre = propietario[0].getText().toString();
             telefono = propietario[1].getText().toString();
             email = propietario[2].getText().toString();
             fecha = propietario[3].getText().toString();
-            face = propietario[4].getText().toString();
+            face = propietario[4].getText().toString();*/
 
             denom = negocio[0].getText().toString();
             giro = negocio[1].getText().toString();
-            producto = negocio[2].getText().toString();
+            descrip = negocio[2].getText().toString();
+            producto = negocio[3].getText().toString();
 
             calle = domicilio[0].getText().toString();
             noI = domicilio[1].getText().toString();
@@ -117,10 +118,9 @@ public class insNegocio extends Fragment implements CheckBox.OnClickListener {
             faceNe = faceN.getText().toString();
             instaNe = instaN.getText().toString();
 
-            if(!nombre.isEmpty() && !telefono.isEmpty() && !email.isEmpty() && !fecha.isEmpty() &&
-            !denom.isEmpty() && !giro.isEmpty() && !producto.isEmpty() &&
+            if(!denom.isEmpty() && !giro.isEmpty() &&
             !calle.isEmpty() && !noI.isEmpty() && !colonia.isEmpty() && !codigo.isEmpty() && !munic.isEmpty() && !estado.isEmpty() &&
-            !emailN.isEmpty() && !telefonoN.isEmpty() && !horaA.isEmpty() && !dias.isEmpty()){
+            !emailN.isEmpty() && !telefonoN.isEmpty()){
 
                 Intent intent = new Intent(getContext(),MainActivity.class);
                 startActivity(intent);
